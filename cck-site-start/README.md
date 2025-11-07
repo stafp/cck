96c49104a2bf88fbbc41b3363e950f8502d648d4
parent <staf.parent@student.howest.be>
2025-11-07 09:17:04 +0100

    init

---
+++ cck-site-start/README.md
@@ -0,0 +1,13 @@
+# CCK
+
+## Design
+
+<https://www.figma.com/design/Qepjp5d40858EedDBt96Yu/CCK?node-id=403-244&t=3JWw8tYXLN9YaCl4-0>
+
+### orginal credits
+
+<https://www.figma.com/design/qW7Tw8hShAFHv5dTmii0oX/Positivus-Landing-Page-Design-(Community)?node-id=25-145&t=LUJ628fwqGa3p4g6-0>
+
+### Illustrations
+
+<https://pixels.market/illustrations/doodledo?c=technology>
---
+++ cck-site-start/src/content.config.ts
@@ -0,0 +1,37 @@
+import { glob } from "astro/loaders";
+import { defineCollection, z } from "astro:content";
+
+const workshopsCollection = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/workshops" }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      image: image(),
+    }),
+});
+
+const teamCollection = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/team" }),
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      function: z.string(),
+      bio: z.string(),
+      pic: image(),
+    }),
+});
+
+const foundationsCollection = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/foundations" }),
+  schema: () =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+    }),
+});
+
+export const collections = {
+  team: teamCollection,
+  foundations: foundationsCollection,
+  workshops: workshopsCollection,
+};
